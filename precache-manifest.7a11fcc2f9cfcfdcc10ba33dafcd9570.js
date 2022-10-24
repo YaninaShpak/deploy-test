7c44aaa1c73d7f2c99ea0a0e9ef4b82ee1b1e36a
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-10-24 21:38:54 +0300

    Updates

---
+++ precache-manifest.7a11fcc2f9cfcfdcc10ba33dafcd9570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc010633c223b07a8e4a16cb696c5c38",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "9c54fb606751170eeb56",
+    "url": "/deploy-test/static/css/main.d9a8cd82.chunk.css"
+  },
+  {
+    "revision": "98802d54cb23fe31ba1c",
+    "url": "/deploy-test/static/js/2.79f22fe1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deploy-test/static/js/2.79f22fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c54fb606751170eeb56",
+    "url": "/deploy-test/static/js/main.ce5870ac.chunk.js"
+  },
+  {
+    "revision": "a3686ea52cf6c04af1d4",
+    "url": "/deploy-test/static/js/runtime-main.5c955694.js"
+  }
+]);
