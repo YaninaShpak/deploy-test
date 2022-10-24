99916d75a17e45dca37ad0f3047ca1084fda19d9
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-10-24 19:41:44 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
       text: 'Разработать ToDo на ReactJS',
       completed: false,
     },
+    {
+      text: 'Деплой',
+      completed: false,
+    },
   ]);
 
   const onToggleCompleted = (index) => {
